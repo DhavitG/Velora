db52350d10f3b96c956e0e16d0a1af4dce05706f
Dhavit Gandhi <gdhavit@gmail.com>
2025-07-17 15:30:34 +0530

    Database connection

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 
+import connectDB from "./mongodb/connect.js";
+
 dotenv.config();
 
 const app = express();
@@ -9,13 +11,19 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
-app.get("/", async(req, res) => {
-    res.send("Hello world")
-})
+app.get("/", async (req, res) => {
+  res.send("Hello world");
+});
 
 const startServer = async () => {
-    app.listen(8080, () => console.log(`Server is running at port http://localhost:8080...`)
-    )
-}
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () =>
+      console.log(`Server is running at port http://localhost:8080...`)
+    );
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 startServer();
