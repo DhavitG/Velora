c2518a14c6ed9164cb5e95dff15cba34c9e052d4
Dhavit Gandhi <gdhavit@gmail.com>
2025-07-18 09:40:06 +0530

    Search functionality added - marking project as complete

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,15 +2,62 @@ import React, { useState, useEffect } from "react";
 import { Loader, Card, FormField } from "../Components";
 
 const RenderCards = ({ data, title }) => {
-  if (data?.length > 0) return data.map((post) => <Card key={post._id} {...post}/>);
+  if (data?.length > 0)
+    return data.map((post) => <Card key={post._id} {...post} />);
 
-  return <h2 className="font-bold mt-5 text-[#6469ff] text-xl uppercase">{title}</h2>
+  return (
+    <h2 className="font-bold mt-5 text-[#6469ff] text-xl uppercase">{title}</h2>
+  );
 };
 
 function Home() {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
   const [searchText, setSearchText] = useState("");
+  const [searchedResults, setSearchedResults] = useState(null);
+  const [searchTimeout, setSearchTimeout] = useState(null);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch("http://localhost:8080/api/v1/post", {
+          method: "GET",
+          headers: {
+            "Content-type": "application/json",
+          },
+        });
+        if (response.ok) {
+          const result = await response.json();
+
+          setAllPosts(result.data.reverse());
+        }
+      } catch (error) {
+        alert(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  function handleSearchChange(e) {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResults = allPosts.filter(
+          (item) =>
+            item.name.toLowerCase().includes(searchText.toLowerCase()) ||
+            item.prompt.toLowerCase().includes(searchText.toLowerCase())
+        );
+
+        setSearchedResults(searchResults);
+      }, 500)
+    );
+  }
 
   return (
     <section className="max-w-7xl mx-auto">
@@ -25,7 +72,14 @@ function Home() {
       </div>
 
       <div className="mt-16">
-        <FormField />
+        <FormField
+          LabelName="Search Posts"
+          type="text"
+          name="text"
+          placeholder="Search posts"
+          value={searchText}
+          handleChange={handleSearchChange}
+        />
       </div>
 
       <div className="mt-10">
@@ -42,13 +96,11 @@ function Home() {
               </h2>
             )}
             <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
-              {
-                searchText ? (
-                  <RenderCards data={[]} title="No search results found"/>
-                ) : (
-                  <RenderCards data={[]} title="No posts found"/>
-                )
-              }
+              {searchText ? (
+                <RenderCards data={searchedResults} title="No search results found" />
+              ) : (
+                <RenderCards data={allPosts} title="No posts found" />
+              )}
             </div>
           </>
         )}
